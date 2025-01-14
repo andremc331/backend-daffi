974621c57719a309b5ff2513d7576bb18c1d2395
andremc331 <andremc331@gmail.com>
2025-01-14 13:49:35 -0300

    build

--- back/src/config/database.ts
+++ back/src/config/database.ts
@@ -2,24 +2,29 @@ import { Sequelize } from 'sequelize';
 import { Orcamento } from '../models/orcamentoModel';
 import { OrcamentoItem } from '../models/orcamentoitemModel';
 
+// Carregar as variáveis de ambiente (considerando o uso de dotenv ou algo semelhante)
+import dotenv from 'dotenv';
+dotenv.config();
+
 // Criação da instância do Sequelize para se conectar ao PostgreSQL
 const sequelize = new Sequelize({
   dialect: 'postgres',
-  host: 'daffi.cdyucqc0y8kr.sa-east-1.rds.amazonaws.com',
+  host: process.env.DB_HOST, // Exemplo de uso de variável de ambiente
   port: 5432,
-  username: 'postgres',
-  password: 'Andre01*',
-  database: 'daffi',
+  username: process.env.DB_USER, // Exemplo de uso de variável de ambiente
+  password: process.env.DB_PASSWORD, // Exemplo de uso de variável de ambiente
+  database: process.env.DB_NAME, // Exemplo de uso de variável de ambiente
   logging: false,
   dialectOptions: {
-    connectTimeout: 10000,  // tempo limite de 10 segundos para conectar
+    connectTimeout: 60000,  // tempo limite de 10 segundos para conectar
   },
 });
 
 // Testar a conexão
 sequelize.authenticate()
-  .then(() => {
-  })
+.then(() => {
+  console.log('Conexão com o banco de dados estabelecida com sucesso!');
+})
   .catch((err) => {
     console.error('Erro ao conectar com o banco de dados:', err);
   });
