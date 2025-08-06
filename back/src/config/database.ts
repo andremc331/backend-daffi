a7d1b480db486c60265c4d410c6e08e0326e15c2
andremc331 <andremc331@gmail.com>
2025-08-06 15:54:49 -0300

    credentials updated

--- back/src/config/database.ts
+++ back/src/config/database.ts
@@ -21,18 +21,24 @@ dotenv.config();
 // });
 
 // Criação da instância do Sequelize para se conectar ao PostgreSQL
-const sequelize = new Sequelize({
-  dialect: process.env.DB_DIALECT as any,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  logging: false,
-  dialectOptions: {
-    connectTimeout: 10000,
-  },
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME!,
+  process.env.DB_USER!,
+  process.env.DB_PASSWORD!,
+  {
+    host: process.env.DB_HOST!,
+    dialect: 'postgres',
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+    logging: false,
+    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
 
 // Testar a conexão
 sequelize.authenticate()
